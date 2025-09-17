defaa2ff97fbbf4a195a41eba6fb5179c205bccd
EthalexG <ethan.gomez001@albright.edu>
2025-09-17 08:47:40 -0400

    Assignment 4 COMPLETE

---
+++ CSC141/Scratch.py
@@ -0,0 +1,7 @@
+evens=[n for n in range(2,21)][0::2]
+print(evens)
+evens = [n * 2 for n in range(1,11)]
+print(evens)
+evems = [n for n in range(2,21)][0::2]
+print(evens)]
+evens = [n for n in range(2.2 if n % 2 === 3]
